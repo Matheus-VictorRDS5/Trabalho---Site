c0db608729c21423b148ab9e2dd3f8ea17ab5686
Matheus-VictorRDS5 <39931@raphaeldisanto.com.br>
2026-04-17 15:32:21 -0300

    Add files via upload

---
+++ Trabalhos programacoes (v1)/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <main>
+        <header>
+          <h1>Relogio - Anoite</h1>
+          <p>03/7/2026</p>
+        </header>
+        <div className="fundo"><img src="" alt="" /></div>
+        <div className="quasefooter">
+          <h1></h1>
+          <p></p>
+          <div className="card">
+          </div>
+          <footer>
+            <div className="icons"></div>
+            <div className="icons"></div>
+            <div className="icons"></div>
+          </footer>
+        </div>
+      </main>
+    </>
+  )
+}
